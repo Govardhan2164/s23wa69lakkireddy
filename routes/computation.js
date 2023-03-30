1ed9b71a60af3a7f4603cc7915e8824e1c41c7c7
Govardhan2164 <govardhan2164@gmail.com>
2023-03-29 20:29:17 -0500

    Create computation.js

---
+++ routes/computation.js
@@ -0,0 +1,40 @@
+var express = require('express');
+
+var router = express.Router();
+/* GET home page. */
+
+let x;
+let y;
+
+router.get('/', function(req, res, next) {
+
+if(req.query.x ==undefined ) {
+
+    x=Math.floor(Math.random()*10);
+
+    y=Math.fround(Math.random());
+
+    let y1=Math.abs(x,y);
+
+    let y2=Math.acos(y);
+
+    let y3=Math.sin(x);
+
+    res.send('Math.abs() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.acos() applied to '+y+' is '+y2+"<br>"+'Math.sin() applied to '+x+' is'+y3);
+    }
+
+else{
+
+    x = req.query.x;
+
+    let y1=Math.abs(x,x);
+
+    let y2=Math.acos(x);
+
+    let y3=Math.sin(x);
+    res.send('Math.abs() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.acos() applied to '+y+' is '+y2+"<br>"+'Math.sin() applied to '+x+' is'+y3);
+
+        }
+
+    });
+module.exports = router;
